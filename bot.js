2967ca42fb8ab047974e9fe926fb5b5849e72403
hhoo1645 <43479514+hhoo1645@users.noreply.github.com>
2018-10-03 23:44:31 +0200

    Update and rename BOT.js to bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
-﻿const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log('I am ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+      }
 });
 
 client.on('ready',  () => {
@@ -1263,4 +1263,5 @@ client.on("guildMemberAdd", member => {
 }).catch(console.error)
 });
 
-client.login('NDg1NjQ3ODYxNTczMjIyNDIx.DoHHLg.ricPAXEBCIrvAjxeRglAXwZhQFE')
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
